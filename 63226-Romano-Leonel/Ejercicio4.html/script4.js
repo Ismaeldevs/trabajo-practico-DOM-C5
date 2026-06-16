2280cde5afac9c9b3d9b2514e6c5a63931504c47
Leonel Romano <leonelromano551@gmail.com>
2026-06-16 20:14:30 -0300

    trabajo practico resuelto

---
+++ 63226-Romano-Leonel/Ejercicio4.html/script4.js
@@ -0,0 +1,12 @@
+const inputNombre = document.getElementById("inputNombre");
+const preview = document.getElementById("preview");
+
+inputNombre.addEventListener("input", function () {
+
+    if (inputNombre.value.trim() === "") {
+        preview.textContent = "Escribí tu nombre arriba.";
+    } else {
+        preview.textContent = `Hola, ${inputNombre.value}!`;
+    }
+
+});
