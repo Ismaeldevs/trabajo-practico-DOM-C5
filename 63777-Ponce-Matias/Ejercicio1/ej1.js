c08cc8a9fb427d5506d60f9c616ed424d7d0573c
Matias Ponce <mati8ponce@gmail.com>
2026-06-16 20:18:54 -0300

    Trabajo Practico Resuelto

---
+++ 63777-Ponce-Matias/Ejercicio1/ej1.js
@@ -0,0 +1,10 @@
+const titulo = document.getElementById("titulo");
+const btnCambiar = document.getElementById("btnCambiar");
+
+btnCambiar.addEventListener("click", function () {
+
+    titulo.textContent = "¡Bienvenido al DOM!";
+
+    titulo.style.color = "navy";
+
+});
---
+++ 63777-Ponce-Matias/Ejercicio6/ej6.js
@@ -0,0 +1,15 @@
+const tarjeta = document.getElementById("tarjeta");
+const btnToggle = document.getElementById("btnToggle");
+
+btnToggle.addEventListener("click", function () {
+
+    const oculto = tarjeta.classList.toggle("oculto");
+
+    if (oculto) {
+        btnToggle.textContent = "Mostrar";
+    }
+    else {
+        btnToggle.textContent = "Ocultar";
+    }
+
+});
