dee451d21489c9a5a0d98eb53ee9fbed1e3ef2e7
Ismaeldevs <PedroIsmaelChavez@alu.frt.utn.edu.ar>
2026-06-18 11:11:28 -0300

    fix: dupe

--- 63481-Calderon-Nicolas/ej1.js
+++
@@ -1,7 +0,0 @@
-const titulo = document.getElementById("titulo");
-const btnCambiar = document.getElementById("btnCambiar");
-
-btnCambiar.addEventListener("click", () => {
-    titulo.textContent = "Titulo cambiado";
-    titulo.style.color = "blue";
-});
